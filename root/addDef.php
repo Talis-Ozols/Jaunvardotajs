6d23f7b073874f88393b14800bf1ab3cd18255f8
FlyeR* <70796316+martinscaune@users.noreply.github.com>
2023-03-21 11:40:28 +0200

    daži error un AddDef, AddDefAction, AddWord

--- root/addDef.php
+++ root/addDef.php
@@ -1,60 +1,55 @@
-<meta charset=utf8>
-
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Jaunvārdotājs | Pievienot definīciju</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<?php 
 	session_start();
-	if (!isset($_SESSION['loggedin']))
-	{
+	if (!isset($_SESSION['loggedin'])) {
 		header("Location: loginForm.php");
 		exit;
 	}
-	
-	echo nl2br("\n");
-	echo "<div style='text-align:right'>";
-	echo 'Sveicināti, ' . $_SESSION['name'] . '!';
-	echo nl2br("\n");
-	echo '<a id="spiedSeit" href="profils.php">Profils</a><br>';
-	echo "<div style='text-align:center'>";
-	echo "<h1>Jaunvārdotājs</h1><br><br>";
-?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html><head>
-  
-  <meta content="text/html; charset=utf8" http-equiv="content-type">
-  <title>Jaunvārdotājs | Pievienot definīciju</title>
-
-  
-<link rel="stylesheet" type="text/css" rel="noopener" target="_blank" href="style.css">
-
-
-</head><body>
-	<?php if (isset($_GET['generetais_vards'])):
-		echo "<p>Vārds ".$_GET['generetais_vards']." veiksmīgi pievienots datubāzei! Varat tam uzreiz pievienot definīciju.</p><br>\n";
-	endif; ?>
-	<form action="addDefAction.php" method="post" name="wordEditForm">Ierakstiet v&#257;rdu<span style="font-weight: bold;"><br>
-  <input maxlength="50" size="40" name="vards"
-	<?php
-		if (isset($_GET['generetais_vards'])) {
-  		echo "value=".$_GET['generetais_vards'];
-		}
-		elseif (isset($_GET['definejamais_vards'])) {
-			echo "value=".$_GET['definejamais_vards'];
-		}
 	?>
-	required><br>
-  <br>
-  </span>Ierakstiet savu defin&#299;ciju<br>
-  <textarea name="definicija" rows="5" cols="40" required></textarea><br>
-  <br>
-  <input value="Apstiprin&#257;t" type="submit"><br>
-</form>
-<br>
-<a id='spiedSeit' href='index.php'>Atpakaļ</a>
-<br><br>
-<?php if (isset($_GET['error']) && $_GET['error'] == "neeksiste"): ?>
-  <span style="font-weight: bold;">Vārds neeksistē!</span>
-<?php endif; ?>
-<?php if (isset($_GET['success']) && $_GET['success'] == "ir"): ?>
-  <span style="font-weight: bold;">Definīcija veiksmīgi pievienota!</span>
-<?php endif; ?>
-</body></html>
+	<div style="text-align: right">
+		<?php echo 'Sveicināti, ' . $_SESSION['name'] . '!'; ?>
+		<br>
+		<a href="profils.php">Profils</a>
+	</div>
+	<div style="text-align: center">
+		<h1>Jaunvārdotājs</h1>
+		<br><br>
+	</div>
+	<?php if (isset($_GET['generetais_vards'])): ?>
+		<p>Vārds <?php echo $_GET['generetais_vards']; ?> veiksmīgi pievienots datubāzei! Varat tam uzreiz pievienot definīciju.</p>
+		<br>
+	<?php endif; ?>
+	<form action="addDefAction.php" method="post" name="wordEditForm">
+		Ierakstiet vārdu
+		<span style="font-weight: bold;"><br>
+			<input maxlength="50" size="40" name="vards" <?php if (isset($_GET['generetais_vards'])) { echo "value=".$_GET['generetais_vards']; } elseif (isset($_GET['definejamais_vards'])) { echo "value=".$_GET['definejamais_vards']; } ?> required>
+			<br>
+			<br>
+		</span>
+		Ierakstiet savu definīciju
+		<br>
+		<textarea name="definicija" rows="5" cols="40" required></textarea>
+		<br>
+		<br>
+		<input value="Apstiprināt" type="submit">
+		<br>
+	</form>
+	<br>
+	<a id="spiedSeit" href="index.php">Atpakaļ</a>
+	<br>
+	<br>
+	<?php if (isset($_GET['error']) && $_GET['error'] == "neeksiste"): ?>
+		<span style="font-weight: bold;">Vārds neeksistē!</span>
+	<?php endif; ?>
+	<?php if (isset($_GET['success']) && $_GET['success'] == "ir"): ?>
+		<span style="font-weight: bold;">Definīcija veiksmīgi pievienota!</span>
+	<?php endif; ?>
+</body>
+</html>
--- root/addWord.php
+++ root/addWord.php
@@ -2,11 +2,11 @@
 
 <?php
 	session_start();
-	if (!isset($_SESSION['loggedin']))
-	{
+	if (!isset($_SESSION['loggedin'])) {
 		header("Location: loginForm.php");
 		exit;
 	}
+	
 	echo nl2br("\n");
 	echo "<div style='text-align:right'>";
 	echo 'Sveicināti, ' . $_SESSION['name'] . '!';
@@ -18,42 +18,39 @@
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html><head>
-  
-  <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
-  <title>Jaunvārdotājs | Pievienot vārdu</title>
+<html>
+<head>
+	<meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
+	<title>Jaunvārdotājs | Pievienot vārdu</title>
+	<link rel="stylesheet" type="text/css" rel="noopener" target="_blank" href="style.css">
+</head>
+<body>
+	<form method="get" action="addWord.php" name="generateWords">
+		Izvēlieties, cik neparastus vārdus ģenerēt:<br>
+		<input type="radio" id="html" name="virknes_garums" value="3" <?php if ((!isset($_GET['virknes_garums'])) or $_GET['virknes_garums'] == 3): echo 'checked'; endif; ?> >
+		<label for="html">"Radoši"</label><br>
+		<input type="radio" id="css" name="virknes_garums" value="4" <?php if (isset($_GET['virknes_garums']) and $_GET['virknes_garums'] == 4): echo 'checked'; endif; ?> >
+		<label for="css">"Reālistiski"</label><br>
+		<br>
+		<input value="&#290;ener&#275;t" type="submit"><br>
+	</form>
+	<a id='spiedSeit' href='index.php'>Atpakaļ</a>
+	<br><br>
 
-<link rel="stylesheet" type="text/css" rel="noopener" target="_blank" href="style.css">
+	<?php
+		include 'generator/generateWords.php';
 
-</head><body>
-<!-- <form method="get" action="addWordAction.php" name="addWord">V&#257;rda garums (Ievadiet skaitli no 1 -2, kur 1 - &#299;sa, 2 - gara)<br> -->
-<!-- <iframe name="dummy_iframe" style="display:none;"></iframe> -->
-<form method="get" action="addWord.php" name="generateWords">Izvēlieties, cik neparastus vārdus ģenerēt:<br>
-	<input type="radio" id="html" name="virknes_garums" value="3" <?php if ((!isset($_GET['virknes_garums'])) or $_GET['virknes_garums'] == 3): echo 'checked'; endif; ?> >
-	<label for="html">"Radoši"</label><br>
-	<input type="radio" id="css" name="virknes_garums" value="4" <?php if (isset($_GET['virknes_garums']) and $_GET['virknes_garums'] == 4): echo 'checked'; endif; ?> >
-	<label for="css">"Reālistiski"</label><br>
-  <br>
-  <input value="&#290;ener&#275;t" type="submit"><br>
-</form>
-<a id='spiedSeit' href='index.php'>Atpakaļ</a>
-<br><br>
-
-<?php
-	
-	include 'generator/generateWords.php';
-
-	// var_dump($word_array);
-	
-	echo "<table style='margin-left: auto; margin-right: auto; font-size:30px'>";
-	echo "<tr> <th>npk</th> <th>jaunvārds</th> </tr>";
-	foreach ($word_array as $i => $word) {
-		echo "<tr> <td style='width:10vw; vertical-align: top;'>".($i+1)."</td> <td style='width:70vw; vertical-align: top;'><form method='get' action='addWordAction.php' name='addWord'><input name='pievienojamais_vards' value='$word' type='submit' class='unstyled_submit_button'></form></td> </tr>";
-	}
-	echo "</table>";
-	
-	// $default_chunk_length
-	
-?>
-
-</body></html>
+		// var_dump($word_array);
+		
+		echo "<table style='margin-left: auto; margin-right: auto; font-size:30px'>";
+		echo "<tr> <th>npk</th> <th>jaunvārds</th> </tr>";
+		foreach ($word_array as $i => $word) {
+			echo "<tr> <td style='width:10vw; vertical-align: top;'>".($i+1)."</td> <td style='width:70vw; vertical-align: top;'><form method='get' action='addWordAction.php' name='addWord'><input name='pievienojamais_vards' value='$word' type='submit' class='unstyled_submit_button'></form></td> </tr>";
+		}
+		echo "</table>";
+		
+		// $default_chunk_length
+		
+	?>
+</body>
+</html>
